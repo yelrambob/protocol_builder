0147fef36f3be11d21f893b4a9c5251ab95e4345
yelrambob <sean.chinery@gmail.com>
2026-07-20 09:25:41 -0400

    Add Metadata class with properties and placeholders

---
+++ metadata.java
@@ -0,0 +1,18 @@
+package com.protocolbook.model;
+
+public class Metadata {
+
+    private String name;
+
+    private String category;
+
+    private String bodyPart;
+
+    private String version;
+
+    private String scanner;
+
+    private String clinicalIndication;
+
+    // getters and setters
+}
