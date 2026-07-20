d639739cdeacbb230b5ced80f74686ef95a8e5a0
yelrambob <sean.chinery@gmail.com>
2026-07-20 09:27:40 -0400

    Create Reconstruction class with fields and no methods

---
+++ reconstruction.java
@@ -0,0 +1,16 @@
+package com.protocolbook.model;
+
+public class Reconstruction {
+
+    private String name;
+
+    private String kernel;
+
+    private String thickness;
+
+    private String interval;
+
+    private String plane;
+
+    // getters/setters
+}
