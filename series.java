92e16184e83efdf7e7adf22d643f9f41c68e28c9
yelrambob <sean.chinery@gmail.com>
2026-07-20 09:27:09 -0400

    Add Series class with properties and initializations

---
+++ series.java
@@ -0,0 +1,20 @@
+package com.protocolbook.model;
+
+import java.util.*;
+
+public class Series {
+
+    private int number;
+
+    private String name;
+
+    private String description;
+
+    private boolean derived;
+
+    private Acquisition acquisition;
+
+    private List<Reconstruction> reconstructions = new ArrayList<>();
+
+    // getters/setters
+}
